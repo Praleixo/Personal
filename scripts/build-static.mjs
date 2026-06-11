93e5ccd2bea6d081f79e53859d39246908e5b8d2
Praleixo <praleixo10@gmail.com>
2026-06-11 00:18:15 -0300

    Atualiza favicons e serviços

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -8,8 +8,15 @@ const files = [
   "index.html",
   "style.css",
   "script.js",
+  "android-chrome-192x192.png",
+  "android-chrome-512x512.png",
+  "apple-touch-icon.png",
+  "favicon-16x16.png",
+  "favicon-32x32.png",
+  "favicon.ico",
   "favicon.svg",
   "robots.txt",
+  "site.webmanifest",
   "sitemap.xml",
 ];
 
